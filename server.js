c5767f2719f349fd7fc5e9fa503dbe3ee2937ed3
Tyagi352 <arnavtyagi116@gmail.com>
2026-07-09 18:57:00 +0530

    url corrected

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+    origin: ['https://sql-studio-frontend-seven.vercel.app'],
     credentials: true,
 }));
 
